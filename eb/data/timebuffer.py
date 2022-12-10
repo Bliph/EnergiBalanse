766c7dc38a03748ba70ea88339bd298cd6185352
Bliph <johan.forsvoll@gmail.com>
2022-12-10 16:02:24 +0100

    Backup

---
+++ eb/data/timebuffer.py
@@ -0,0 +1,160 @@
+
+class TimeBuffer:
+
+    def __init__(self):
+        self.sorted_list = []
+
+    #################################################################
+    # Find index for tuple with ts >= provided ts
+    #
+    def get_index(self, ts: int, from_idx=0, to_idx=0):
+
+        from_idx = max(from_idx, 0)
+        to_idx = min(to_idx, len(self.sorted_list)-1)
+        
+        # Empty list: 
+        if len(self.sorted_list) <= 0:
+            return 0
+
+        # Default: whole list
+        if to_idx <= 0:
+            to_idx = len(self.sorted_list) - 1 
+
+        # Before first element
+        if ts <= self.sorted_list[from_idx][0]:
+            return from_idx
+        
+        # After last element
+        elif ts > self.sorted_list[to_idx][0]:
+            return to_idx + 1
+        
+        # Smallest possible interval (int)
+        elif to_idx - from_idx <= 1:
+            return to_idx
+
+        # Rcurse
+        else:
+
+            # Middle index
+            m_idx = from_idx + int((to_idx - from_idx)/2)
+            
+            # Spot on
+            if ts == self.sorted_list[m_idx][0]:
+                return m_idx
+            
+            # Lower half
+            elif ts < self.sorted_list[m_idx][0]:
+                return self.get_index(ts, from_idx=from_idx, to_idx=m_idx)
+            
+            # Upper half
+            elif ts > self.sorted_list[m_idx][0]:
+                return self.get_index(ts, from_idx=m_idx, to_idx=to_idx)
+
+    #################################################################
+    # Insert a element and keep list sorted by ts
+    #
+    def insert_sorted(self, ts: int, value, overwrite=True):
+        vt = (ts, value)
+        idx = self.get_index(ts)
+
+        if overwrite and \
+            len(self.sorted_list) > 0 and \
+            idx < len(self.sorted_list) and \
+            idx > 0 and \
+            self.sorted_list[idx][0] == ts:
+            self.sorted_list[idx] = vt
+        else:
+            self.sorted_list.insert(idx, vt)
+
+    #################################################################
+    # Return list [from, to], including
+    #
+    def get_interval(self, from_ts=0, to_ts=0):
+        if len(self.sorted_list) <= 0:
+            return []
+
+        # Default: whole list
+        if to_ts <= 0:
+            to_ts = a.sorted_list[-1:][0][0]
+
+        from_idx = self.get_index(ts=from_ts)
+        to_idx = self.get_index(ts=to_ts)
+        
+        # If to_ts is at last element of range, include last element
+        if to_idx < len(self.sorted_list) and to_ts == a.sorted_list[to_idx][0]:
+            to_idx += 1
+
+        return self.sorted_list[from_idx:to_idx]
+
+    #################################################################
+    # Crop list [from, to], including
+    #
+    def crop_interval(self, from_ts=0, to_ts=0):
+        self.sorted_list = self.get_interval(from_ts=from_ts, to_ts=to_ts)
+
+    #################################################################
+    # Get value
+    # * 'inter'
+    # * 'pre'
+    # * 'post'
+    #
+    def get_value(self, ts: int, selection='inter'):
+        if len(self.sorted_list) <= 0:
+            return 0
+
+        idx = self.get_index(ts=ts)
+        pre_idx = max(0, idx-1)
+        post_idx = max(0, min(idx, len(self.sorted_list)-1))
+
+        pre = self.sorted_list[pre_idx]
+        post = self.sorted_list[post_idx]
+
+        if selection.lower() == 'pre':
+            return pre[1]
+        elif selection.lower() == 'post':
+            return post[1]
+        elif selection.lower() == 'avg':
+            return (post[1]+pre[1])/2
+        elif selection.lower() == 'inter':
+            dt = post[0]-pre[0]
+            dv = post[1]-pre[1]
+            if dt == 0:
+                return pre[1]
+            else:
+                return pre[1] + dv*(ts-pre[0])/dt
+        else:
+            return 0
+
+import time
+import random
+
+a = TimeBuffer() 
+a.insert_sorted(ts = int(time.time()*1000), value='a') 
+time.sleep(random.random())
+a.insert_sorted(ts = int(time.time()*1000), value='b') 
+time.sleep(random.random())
+a.insert_sorted(ts = int(time.time()*1000), value='c')
+
+aa = a.get_index(a.sorted_list[1][0]-1)
+bb = a.get_index(a.sorted_list[1][0])
+cc = a.get_index(a.sorted_list[1][0]+1)
+
+a.insert_sorted(ts=a.sorted_list[1][0]+1, value='b_post')
+a.insert_sorted(ts=a.sorted_list[1][0], value='b_at')
+a.insert_sorted(ts=a.sorted_list[1][0]-1, value='b_pre')
+
+x = a.get_index(ts=a.sorted_list[-1:][0][0])
+
+l = a.get_interval()
+l = a.get_interval(from_ts=a.sorted_list[1][0], to_ts=a.sorted_list[1][0])
+l = a.get_interval(from_ts=a.sorted_list[1][0], to_ts=a.sorted_list[2][0])
+l = a.get_interval(from_ts=a.sorted_list[1][0], to_ts=a.sorted_list[3][0])
+
+a.sorted_list = []
+l = a.get_interval(10, 20)
+
+a.insert_sorted(ts=30, value='x')
+l = a.get_interval(10, 20)
+
+pass
+pass
